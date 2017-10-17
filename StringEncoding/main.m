f62929734e412c8692a27d2c28c4fcbbad9b31b6
sergeybut <edg3@ukr.net>
2017-10-17 16:08:55 +0300

    Initial Commit

---
+++ StringEncoding/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StringEncoding
+//
+//  Created by Serhii Butenko on 10/17/17.
+//  Copyright © 2017 Serhii Butenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
